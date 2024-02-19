9a97e29356813180dc47727ccc6d168ce002e40f
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-19 18:56:34 +0100

    update delete

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,6 @@
 "use strict";
 class CartItem {
-    constructor(title, qty, price, onSale) {
+    constructor(title, qty, price, onSale = false) {
         this.title = title;
         this.qty = qty;
         this.price = price;
@@ -8,35 +8,41 @@ class CartItem {
         this.id = +Math.random().toString().slice(2);
     }
 }
-const CartArr = [
+let cartArr = [
     new CartItem('BasketBall', 1, 69.99, false),
     new CartItem('Golf Club', 1, 139.99, false),
     new CartItem('Carbon fins', 1, 290.0, false),
 ];
-console.log('CartArr ===', CartArr);
+console.log('cartArr ===', cartArr);
 function addToCart(arr, item) {
     arr.push(item);
 }
-const newItem = {
-    id: 4,
-    title: 'IceCream',
-    qty: 1,
-    price: 5,
-    onSale: false,
-};
-addToCart(CartArr, newItem);
-console.log('CartArr ===', CartArr);
-const newItem2 = {
-    id: 4,
-    title: 'Basball Cap',
-    qty: 1,
-    price: 29.99,
-    onSale: false,
-};
+const newItem = new CartItem('IceCream', 1, 5, false);
+addToCart(cartArr, newItem);
+console.log('CartArr ===', cartArr);
+const newItem2 = new CartItem('Basball Cap', 1, 29.99, false);
 const newItem3 = new CartItem('Baseball', 1, 58.75, false);
-console.log('newItem3 ===', newItem3);
-addToCart(CartArr, newItem2);
-addToCart(CartArr, newItem3);
-function deleteFromCart(arr, idToDelete) { }
-console.log('CartArr ===', CartArr);
+addToCart(cartArr, newItem2);
+addToCart(cartArr, newItem3);
+const id = cartArr[0].id;
+function deleteFromCart(arr, idToDelete) {
+    arr = arr.filter((cObj) => cObj.id !== idToDelete);
+}
+console.log('CartArrBeforeDelete ===', cartArr);
+deleteFromCart(cartArr, id);
+console.log('CartArrAfterDelete ===', cartArr);
+const idToUpdate = cartArr[1].id;
+function updateTite(arr, idToUpdate, newTitle) {
+    arr = arr.map((cObj) => {
+        if (cObj.id === idToUpdate) {
+            return { ...cObj, title: newTitle };
+        }
+        else {
+            return cObj;
+        }
+    });
+    console.log('arr ===', arr);
+}
+updateTite(cartArr, idToUpdate, 'Sporbaciai');
+console.log('cartArrAfterUpdate ===', cartArr);
 //# sourceMappingURL=cart.js.map
--- src/cart.ts
+++ src/cart.ts
@@ -3,7 +3,7 @@ interface CartItemIf {
   title: string;
   qty: number;
   price: number;
-  onSale: boolean;
+  onSale?: boolean;
 }
 
 class CartItem implements CartItemIf {
@@ -13,48 +13,56 @@ class CartItem implements CartItemIf {
     public title: string,
     public qty: number,
     public price: number,
-    public onSale: boolean
+    public onSale: boolean = false
   ) {
     this.id = +Math.random().toString().slice(2);
   }
 }
 
-const CartArr: CartItemIf[] = [
+let cartArr: CartItemIf[] = [
   new CartItem('BasketBall', 1, 69.99, false),
   new CartItem('Golf Club', 1, 139.99, false),
   new CartItem('Carbon fins', 1, 290.0, false),
 ];
 
-console.log('CartArr ===', CartArr);
+console.log('cartArr ===', cartArr);
 
 function addToCart(arr: CartItemIf[], item: CartItemIf): void {
   arr.push(item);
 }
 
-const newItem: CartItemIf = {
-  id: 4,
-  title: 'IceCream',
-  qty: 1,
-  price: 5,
-  onSale: false,
-};
+const newItem: CartItemIf = new CartItem('IceCream', 1, 5, false);
 
-addToCart(CartArr, newItem);
-console.log('CartArr ===', CartArr);
+addToCart(cartArr, newItem);
+console.log('CartArr ===', cartArr);
 
-const newItem2: CartItemIf = {
-  id: 4,
-  title: 'Basball Cap',
-  qty: 1,
-  price: 29.99,
-  onSale: false,
-};
+const newItem2: CartItemIf = new CartItem('Basball Cap', 1, 29.99, false);
 
 const newItem3 = new CartItem('Baseball', 1, 58.75, false);
-console.log('newItem3 ===', newItem3);
 
-addToCart(CartArr, newItem2);
-addToCart(CartArr, newItem3);
+addToCart(cartArr, newItem2);
+addToCart(cartArr, newItem3);
 
-function deleteFromCart(arr: CartItemIf[], idToDelete: number): void {}
-console.log('CartArr ===', CartArr);
+const id = cartArr[0].id;
+function deleteFromCart(arr: CartItemIf[], idToDelete: number): void {
+  arr = arr.filter((cObj) => cObj.id !== idToDelete);
+}
+console.log('CartArrBeforeDelete ===', cartArr);
+deleteFromCart(cartArr, id);
+console.log('CartArrAfterDelete ===', cartArr);
+
+const idToUpdate = cartArr[1].id;
+function updateTite(arr: CartItemIf[], idToUpdate: number, newTitle: string) {
+  arr = arr.map((cObj) => {
+    if (cObj.id === idToUpdate) {
+      return { ...cObj, title: newTitle };
+    } else {
+      return cObj;
+    }
+  });
+  console.log('arr ===', arr);
+}
+updateTite(cartArr, idToUpdate, 'Sporbaciai');
+console.log('cartArrAfterUpdate ===', cartArr);
+
+// pasidaryti user interfaice ui kad matytusi ant html
